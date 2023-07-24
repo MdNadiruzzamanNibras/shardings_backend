9ab22685871909ba02ef2e72709c648ffafcad60
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-24 16:19:53 +0600

    chat add

--- index.js
+++ index.js
@@ -26,10 +26,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     client.connect();
+
+      const MeetCollection = client.db('Meet').collection('massage');
+      
+      app.post('/chats', async(req,res)=>{
+          const chat = req.body 
+          console.log(chat, "chat");
+          const result = await MeetCollection.insertOne(chat)
+          console.log(result, 'result');
+      res.send(result)
+    })
+    
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
